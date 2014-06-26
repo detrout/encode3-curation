4298bea8b5567c278d684430b68504763cbd4fe7
Diane Trout <diane@ghic.org>
2014-06-26 10:20:47 -0700

    lets move the setup code into a module I can reuse between notebooks

---
+++ curation_common.py
@@ -0,0 +1,32 @@
+import os
+import sys
+
+from IPython import get_ipython
+
+# maybe we should just install htsworkflow
+def custom_paths():
+    paths = [ os.path.expanduser('~/proj/solexa/htsworkflow'),
+              ]
+    for p in paths:
+        if p not in sys.path:
+            sys.path.append(p)
+custom_paths()
+
+from htsworkflow.submission.encoded import ENCODED
+from htsworkflow.util.rdfhelp import get_model, dump_model
+from htsworkflow.util.rdfjsonld import load_into_model
+
+ipython = get_ipython()
+
+ipython.magic('load_ext rdfmagic')
+ipython.magic('%addns htsw http://jumpgate.caltech.edu/wiki/LibraryOntology#')
+ipython.magic('%addns library http://jumpgate.caltech.edu/library/')
+ipython.magic('%addns flowcell http://jumpgate.caltech.edu/flowcell/')
+
+ipython.magic('%addns biosample https://www.encodedcc.org/profiles/biosample.json#')
+ipython.magic('%addns experiment https://www.encodedcc.org/profiles/experiment.json#')
+ipython.magic('%addns library https://www.encodedcc.org/profiles/library.json#')
+ipython.magic('%addns platform https://www.encodedcc.org/profiles/platform.json#')
+ipython.magic('%addns replicates https://www.encodedcc.org/profiles/replicates.json#')
+
+ipython.magic('%addns experiments https://www.encodedcc.org/experiments/')
