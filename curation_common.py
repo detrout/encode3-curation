9f6fddbaf382cfb972052f445e1e084098718c8a
Diane Trout <diane@ghic.org>
2015-06-30 14:19:43 -0700

    add function for building gene id from gtf file

--- curation_common.py
+++ curation_common.py
@@ -1,7 +1,13 @@
 import os
+import gzip
 import sys
+import time
+import logging
 
 from IPython import get_ipython
+
+logger = logging.getLogger(__name__)
+
 
 # maybe we should just install htsworkflow
 def custom_paths():
@@ -34,3 +40,46 @@ ipython.magic('%addns experiments https://www.encodeproject.org/experiments/')
 ipython.magic('%addns libraries https://www.encodeproject.org/libraries/')
 ipython.magic('%addns files https://www.encodeproject.org/files/')
 
+
+def build_geneid_to_gene_from_gtf(gencode):
+    """Build a dictionary mapping from gene_id to gene_name.
+
+    Arguments:
+        gencode (str): compressed filename to read
+
+    Returns:
+        dictionary mapping gene_id to gene_name
+    """
+    logger.info("Loading %s", gencode)
+    start = time.time()
+    names = {}
+    with gzip.GzipFile(gencode, 'r') as instream:
+        for line in instream:
+            line = line.decode('ascii')
+
+            if line.startswith('#'):
+                continue
+
+            columns = line.split('\t')
+
+            gene_id = None
+            gene_name = None
+
+            for item in columns[-1].split(';'):
+                item = item.strip()
+                if len(item) == 0:
+                    continue
+                item = item.split()
+                if len(item) != 2:
+                    print("Confused: {} {}".format(item, len(item)))
+                name, value = item
+                if name == 'gene_id':
+                    gene_id = value[1:-1]
+                elif name == 'gene_name':
+                    gene_name = value[1:-1]
+
+            if gene_id and gene_name:
+                names[gene_id] = gene_name
+
+    logger.info("loaded in %d seconds", time.time() - start)
+    return names
